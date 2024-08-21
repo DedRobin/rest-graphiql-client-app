bbbe19e71b1b66835971ee97fd552c7ceb21a66b
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-21 21:20:09 +0300

    feat: add test for RootLayoyt

--- src/tests/app/layout.test.tsx
+++ src/tests/app/layout.test.tsx
@@ -1,12 +1,29 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import MainPage from "@/app/page";
+import RootLayout from "@/app/layout";
 
-describe.skip("<RootLayout", () => {
-  test("RootLeyout should be defined", () => {
-    render(<MainPage />);
-    expect(
-      screen.getByRole("heading", { name: /my application/i }),
-    ).toBeInTheDocument();
+vi.mock("next/font/google", () => ({
+  Inter: vi.fn(() => ({
+    className: "mock-inter-classname",
+  })),
+}));
+
+describe("<RootLayout", () => {
+  test("RootLayout should be defined", () => {
+    render(
+      <RootLayout>
+        <div>Foo</div>
+      </RootLayout>,
+    );
+    expect(screen.getByText(/Foo/i)).toBeInTheDocument();
+  });
+  test("RootLayout has className='mock-inter-classname'", () => {
+    const { container } = render(
+      <RootLayout>
+        <div>Foo</div>
+      </RootLayout>,
+    );
+    const body = container.querySelector(".mock-inter-classname");
+    expect(body).toBeDefined();
   });
 });

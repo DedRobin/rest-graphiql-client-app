626ae47ebfbef5af9a0af0034bcd9a6ae14cd2f5
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-22 15:16:44 +0300

    add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,58 @@
+import { config as enableEnvVars } from "dotenv";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+enableEnvVars();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export const registerWithEmailAndPassword = async (
+  name: string,
+  email: string,
+  password: string,
+) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert((err as Error).message);
+  }
+};
+
+export const sendPasswordReset = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert((err as Error).message);
+  }
+};
+
+export const logout = () => {
+  signOut(auth);
+};

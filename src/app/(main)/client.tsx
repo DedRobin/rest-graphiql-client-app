f4a2c03eaa66103251bf7e83ae76c52cf6776f0f
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-25 10:21:49 +0300

    feat: add links 'sign in' and sign up' on Home page

--- src/app/(main)/client.tsx
+++ src/app/(main)/client.tsx
@@ -1,5 +1,33 @@
 "use client";
 
+import { auth } from "@/firebase";
+import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 export default function Home() {
-  return <div className="home text-5xl">Home</div>;
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="home flex flex-col gap-2">
+      {user ? (
+        <div className="text-5xl">You are authorized</div>
+      ) : (
+        <>
+          <div className="home-welcome text-5xl">Welcome</div>
+          <Link
+            href="/login"
+            className="sign-in text-center border-2 rounded-full hover:border-lime-400 hover:text-lime-500"
+          >
+            Sign in
+          </Link>
+          <Link
+            href="/register"
+            className="sign-up text-center border-2 rounded-full hover:border-lime-400 hover:text-lime-500"
+          >
+            Sign up
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }

9e75f0657647455403b87c2a0ce65a65f23df306
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-25 13:54:58 +0300

    fix: fix tests

--- src/tests/app/(main)/layout.test.tsx
+++ src/tests/app/(main)/layout.test.tsx
@@ -1,10 +1,15 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import MainLayout from "@/app/(main)/layout";
+
+vi.mock("next/navigation", () => ({
+  useRouter: vi.fn(),
+  usePathname: vi.fn().mockReturnValue("/"),
+}));
 
 describe("<MainLayout", () => {
   test("<MainLayout /> should be in the document", () => {
     render(<MainLayout>Foo</MainLayout>);
-    expect(screen.getByText(/Foo/i)).toBeInTheDocument();
+    expect(screen.getByText("Foo")).toBeInTheDocument();
   });
 });
--- src/tests/app/(main)/register/page.test.tsx
+++ src/tests/app/(main)/register/page.test.tsx
@@ -1,10 +1,26 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import RegisterPage from "@/app/(main)/register/page";
+// import { useAuthState } from "@/tests/mocks/firebase";
+
+vi.mock("next/navigation", () => ({
+  useRouter: vi.fn(),
+}));
+vi.mock("react-firebase-hooks/auth", () => ({
+  useAuthState: vi
+    .fn()
+    .mockImplementationOnce(() => [null, false])
+    .mockImplementationOnce(() => [null, true]),
+}));
 
 describe("<RegisterPage />", () => {
-  test("<RegisterPage /> should be in the document", () => {
+  test("<RegisterPage /> should be in the document (when user is authenticated)", () => {
     render(<RegisterPage />);
-    expect(screen.getByText(/Register/i)).toBeInTheDocument();
+    expect(screen.getByText(/Registration/i)).toBeInTheDocument();
+  });
+
+  test("<RegisterPage /> should be in the document (while is loading)", () => {
+    render(<RegisterPage />);
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
   });
 });

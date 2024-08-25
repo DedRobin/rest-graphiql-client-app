88eb855d0aab99799bf67fce777840da39020d1b
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-25 16:31:31 +0300

    feat: add validation for registration form

--- src/app/(main)/register/client.tsx
+++ src/app/(main)/register/client.tsx
@@ -7,11 +7,14 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import registrationSchema from "./schema";
 
 export type TRegisterForm = {
   name: string;
   email: string;
   password: string;
+  confirmPassword: string;
 };
 
 export default function Register() {
@@ -19,8 +22,11 @@ export default function Register() {
     register,
     handleSubmit,
     // watch,
-    // formState: { errors },
-  } = useForm<TRegisterForm>();
+    formState: { errors },
+  } = useForm<TRegisterForm>({
+    resolver: yupResolver(registrationSchema),
+    mode: "all",
+  });
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
 
@@ -35,38 +41,65 @@ export default function Register() {
         <Loader />
       ) : (
         <form
-          className="login-form flex flex-col gap-3"
+          className="registration-form flex flex-col gap-3"
           onSubmit={handleSubmit(registerWithEmailAndPassword)}
         >
-          <h1 className="login-heading text-5xl text-center my-5">
+          <h1 className="registration-heading text-5xl text-center my-5">
             Registration
           </h1>
-          <div className="name-field flex gap-3 justify-between">
-            <label htmlFor="name">Name</label>
-            <input
-              className="name-input text-black"
-              id="name"
-              type="text"
-              {...register("name")}
-            />
+          <div className="name-field">
+            <div className="name-wrapper flex gap-3 justify-between">
+              <label htmlFor="name">Name</label>
+              <input
+                className="name-input text-black"
+                id="name"
+                type="text"
+                {...register("name")}
+              />
+            </div>
+            <p className="error text-sm text-right text-red-500 min-h-6 ">
+              {errors.name?.message}
+            </p>
           </div>
-          <div className="email-field flex gap-3 justify-between">
-            <label htmlFor="email">Email</label>
-            <input
-              className="email-input text-black"
-              id="email"
-              type="email"
-              {...register("email")}
-            />
+          <div className="email-field">
+            <div className="email-wrapper flex gap-3 justify-between">
+              <label htmlFor="email">Email</label>
+              <input
+                className="email-input text-black"
+                id="email"
+                type="email"
+                {...register("email")}
+              />
+            </div>
+            <p className="error text-sm text-right text-red-500 min-h-6">
+              {errors.email?.message}
+            </p>
           </div>
-          <div className="password-field flex gap-3 justify-between">
-            <label htmlFor="password">Password</label>
-            <input
-              className="password-input text-black"
-              id="password"
-              type="password"
-              {...register("password")}
-            />
+          <div className="password-field flex flex-col">
+            <div className="password-wrapper flex justify-between">
+              <label htmlFor="password">Password</label>
+              <input
+                className="password-input text-black"
+                id="password"
+                type="password"
+                {...register("password")}
+              />
+            </div>
+            <p className="error text-sm text-right text-red-500 min-h-6">
+              {errors.password?.message}
+            </p>
+            <div className="confirm-password-wrapper flex gap-3 justify-between">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                className="confirm-password-input text-black"
+                id="confirm-password"
+                type="password"
+                {...register("confirmPassword")}
+              />
+            </div>
+            <p className="error text-sm text-right text-red-500 min-h-6">
+              {errors.confirmPassword?.message}
+            </p>
           </div>
           <button
             className="sign-in-btn border-2 rounded-full hover:border-lime-400 hover:text-lime-500"
---
+++ src/app/(main)/register/schema.ts
@@ -0,0 +1,35 @@
+import * as yup from "yup";
+
+const registrationSchema = yup
+  .object({
+    name: yup
+      .string()
+      .matches(/^[A-Z].*/, "First letter must be uppercase")
+      .required("Field is required"),
+    email: yup
+      .string()
+      .email("Email must look like 'example@mail.com'")
+      .matches(
+        /\b[A-Za-z0-9._%+-]+[^.]@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/,
+        "Email must look like 'example@mail.com'",
+      )
+      .required("Field is required"),
+    password: yup
+      .string()
+      .min(8, "Must be at least 8 characters")
+      .matches(/[0-9]/, "Must contain one number")
+      .matches(/[A-Z]/, "Must contain one uppercase letter")
+      .matches(/[a-z]/, "Must contain one lowercase letter")
+      .matches(
+        /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/,
+        "Must contain one special characters",
+      )
+      .required("Field is required"),
+    confirmPassword: yup
+      .string()
+      .required("Field is required")
+      .oneOf([yup.ref("password")], "Passwords must match"),
+  })
+  .required();
+
+export default registrationSchema;

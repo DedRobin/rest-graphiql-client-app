dfc228d08f4609544fd32026d9e3d95a1301d235
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-27 14:24:51 +0300

    feat: add fallback message if no requests, add custom hook 'useLocalStorage'

--- src/app/(main)/history/client.tsx
+++ src/app/(main)/history/client.tsx
@@ -1,5 +1,32 @@
 "use client";
 
+import { Route } from "@/app/routes";
+import Link from "next/link";
+import { LocalStorage } from "./constants";
+import { useLocalStorage } from "@/hooks/localStorageHook";
+
 export default function History() {
-  return <div className="history text-5xl">History</div>;
+  const historyRequests = useLocalStorage(LocalStorage.Requests);
+
+  return (
+    <div className="history flex flex-col justify-center gap-3">
+      {historyRequests ? (
+        <div className="history-requests">Here must be some requests</div>
+      ) : (
+        <>
+          <div className="histoty-empty text-5xl">
+            {"You haven't executed any requests yet"}
+          </div>
+          <div className="history-link flex justify-around text-3xl hover:*:text-lime-500">
+            <Link className="history-link-rest" href={Route.RESTfull}>
+              REST Client
+            </Link>
+            <Link className="history-link-graphql" href={Route.GraphiQL}>
+              GraphQL Client
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
---
+++ src/hooks/localStorageHook.ts
@@ -0,0 +1,15 @@
+import { LocalStorage } from "@/app/(main)/history/constants";
+import { useEffect, useState } from "react";
+
+export function useLocalStorage(key: LocalStorage) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem(key);
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, [key]);
+
+  return data;
+}

f5d70de4a44b18dfa0940c7bc17e39bab85a7c2a
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-27 08:49:00 +0300

    fix: fix tests

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./src/tests/setup.ts"],
+    setupFiles: ["./src/__tests__/setup.ts"],
     coverage: {
       include: ["src/**"],
     },

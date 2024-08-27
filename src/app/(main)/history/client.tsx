94f6ea2977b4e814923876ea27e4a723e14c8cdf
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-27 19:29:00 +0300

    feat: add additioanl text

--- src/app/(main)/history/client.tsx
+++ src/app/(main)/history/client.tsx
@@ -14,10 +14,13 @@ export default function History() {
         <div className="history-requests">Here must be some requests</div>
       ) : (
         <>
-          <div className="histoty-empty text-5xl">
-            {"You haven't executed any requests yet"}
+          <div className="histoty-empty text-center text-5xl">
+            <p>{"You haven't executed any requests yet"}</p>
+            <p>{"It's empty here."}</p>
+            <p>{"Try those options:"}</p>
           </div>
-          <div className="history-link flex justify-around text-3xl m-20 *:border-2 *:rounded *:p-2 hover:*:text-lime-500 hover:*:border-lime-400">
+
+          <div className="history-link flex justify-around text-3xl m-10 *:border-2 *:rounded *:p-2 hover:*:text-lime-500 hover:*:border-lime-400">
             <Link className="history-link-rest" href={Route.RESTfull}>
               REST Client
             </Link>

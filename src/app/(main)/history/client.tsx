057eeec9393206ce9df5a410166068415af84601
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-29 11:44:21 +0300

    feat: add style modeule for <History />

--- src/app/(main)/history/client.tsx
+++ src/app/(main)/history/client.tsx
@@ -6,12 +6,13 @@ import { HistoryData } from "@/components/History/Data";
 import { HistoryFallback } from "@/components/History/Fallback";
 import { mockHistoryData } from "./__mocks__/mockHistoryData";
 import { Loader } from "@/components/UI/Loader";
+import styles from "./Style.module.css";
 
 export default function History() {
   const [historyData, setHistoryData] = useLocalStorage(LSKey.HistoryData);
 
   return (
-    <div className="history flex flex-col gap-3 mx-40">
+    <div className={styles.history}>
       {historyData ? (
         <HistoryData historyData={mockHistoryData} />
       ) : historyData === null ? (
@@ -23,14 +24,14 @@ export default function History() {
       {/* Temporary plug that adds fake data */}
       {!historyData ? (
         <button
-          className="text-2xl m-10 border-2 rounded p-2 hover:text-lime-500 hover:border-lime-400 self-center"
+          className={styles.button}
           onClick={() => setHistoryData(mockHistoryData)}
         >
           Add data
         </button>
       ) : (
         <button
-          className="text-2xl m-10 border-2 rounded p-2 hover:text-lime-500 hover:border-lime-400 self-center"
+          className={styles.button}
           onClick={() => setHistoryData(undefined)}
         >
           Clear

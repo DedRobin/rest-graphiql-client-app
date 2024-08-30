fb4568ea0144e2773ab11e6fce1512f4bc355c85
Pavel Rabekin <pavelrabekin@gmail.com>
2024-08-30 09:04:05 +0300

    refactor: remove styled module from history page

--- src/app/(main)/history/client.tsx
+++ src/app/(main)/history/client.tsx
@@ -5,14 +5,13 @@ import { useLocalStorage } from "@/hooks/localStorageHook";
 import { HistoryFallback } from "@/components/History/Fallback";
 import { mockHistoryData } from "./__mocks__/mockHistoryData";
 import { Loader } from "@/components/UI/Loader";
-import styles from "./style.module.css";
 import { HistoryData } from "@/components/History/DataList";
 
 export default function History() {
   const [historyData, setHistoryData] = useLocalStorage(LSKey.HistoryData);
 
   return (
-    <div className={styles.history}>
+    <div className="flex flex-col items-center gap-3 mx-40">
       {historyData ? (
         <HistoryData historyData={mockHistoryData} />
       ) : historyData === null ? (
@@ -23,7 +22,7 @@ export default function History() {
 
       {/* Temporary plug that adds fake data */}
       <button
-        className={styles.button}
+        className="text-2xl m-10 border-2 rounded p-2 hover:text-lime-500 hover:border-lime-400 self-center"
         onClick={() =>
           !historyData
             ? setHistoryData(mockHistoryData)

8c4fe94c3a5149e66e0d073e1cbabf139a373fe4
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-02 21:46:28 +0300

    feat: add valid token handler

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,32 +1,5 @@
-import { notFound } from "next/navigation";
 import { Home } from "./client";
-import { getTokens } from "next-firebase-auth-edge";
-import { cookies } from "next/headers";
-import { clientConfig, serverConfig } from "@/services/firebase";
 
 export default async function HomePage() {
-  const tokens = await getTokens(cookies(), {
-    apiKey: clientConfig.apiKey,
-    cookieName: serverConfig.cookieName,
-    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
-    serviceAccount: serverConfig.serviceAccount,
-  });
-
-  // if (!tokens) {
-  //   notFound();
-  // }
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {" "}
-      <h1 className="text-xl mb-4">Super secure home page</h1>{" "}
-      <p>
-        {" "}
-        Only <strong>{tokens?.decodedToken.email}</strong> holds the magic key
-        to this kingdom!{" "}
-      </p>{" "}
-    </main>
-  );
-
-  // return <Home />;
+  return <Home />;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import { NextRequest } from "next/server";
-import { authMiddleware, redirectToLogin } from "next-firebase-auth-edge";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  authMiddleware,
+  redirectToHome,
+  redirectToLogin,
+} from "next-firebase-auth-edge";
 import { clientConfig, serverConfig } from "./services/firebase";
 import { Route } from "./app/routes";
-
+const PUBLIC_PATHS = [Route.Login, Route.Registration];
 export async function middleware(request: NextRequest) {
   return authMiddleware(request, {
     loginPath: "/api/login",
@@ -12,12 +16,23 @@ export async function middleware(request: NextRequest) {
     cookieSignatureKeys: serverConfig.cookieSignatureKeys,
     cookieSerializeOptions: serverConfig.cookieSerializeOptions,
     serviceAccount: serverConfig.serviceAccount,
+    handleValidToken: async ({}, headers) => {
+      if (PUBLIC_PATHS.includes(request.nextUrl.pathname as Route)) {
+        return redirectToHome(request);
+      }
+
+      return NextResponse.next({
+        request: {
+          headers,
+        },
+      });
+    },
     handleInvalidToken: async (reason) => {
       console.info("Missing or malformed credentials", { reason });
 
       return redirectToLogin(request, {
         path: Route.Login,
-        publicPaths: [Route.Login, Route.Registration, Route.Main],
+        publicPaths: PUBLIC_PATHS.concat([Route.Main]),
       });
     },
   });

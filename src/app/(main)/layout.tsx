164c968856b7deea74f6c41a26737c28c078e49e
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-04 10:56:34 +0300

    feat: add modal

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header/Header";
 import { Main } from "@/components/Main";
+import { Modal } from "@/components/Modal/Modal";
 
 export default function MainLayout({
   children,
@@ -12,6 +13,7 @@ export default function MainLayout({
       <Header />
       <Main>{children}</Main>
       <Footer />
+      <Modal isOpen={true} />
     </>
   );
 }

1284811819c0d1af6508a77150af0a585b04f906
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-08 11:45:43 +0300

    feat: add locale for main page

--- src/components/UI/Navigation/AuthenticatedSidebarNavigation.tsx
+++ src/components/UI/Navigation/AuthenticatedSidebarNavigation.tsx
@@ -3,9 +3,12 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Route } from "@/app/routes";
+import localeData from "@/services/locale/lang.json";
+import { useLocale } from "@/services/locale/contex";
 
 export default function AuthenticatedSidebarNavigation() {
   const pathname = usePathname();
+  const { language } = useLocale();
 
   const isActive = (route: string) => pathname === route;
 
@@ -17,7 +20,7 @@ export default function AuthenticatedSidebarNavigation() {
           isActive(Route.Main) ? "text-lightGray" : ""
         }`}
       >
-        Main
+        {localeData.authenticatedSidebarNavigation.main[language]}
       </Link>
       <Link
         href={Route.RESTfull}
@@ -41,7 +44,7 @@ export default function AuthenticatedSidebarNavigation() {
           isActive(Route.History) ? "text-lightGray" : ""
         }`}
       >
-        History
+        {localeData.authenticatedSidebarNavigation.history[language]}
       </Link>
     </>
   );

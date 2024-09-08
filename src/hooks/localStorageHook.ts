f97be1b4f727458003ed5c049895ce3d30693d19
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-08 09:42:40 +0300

    fix: fix localStorage

--- src/hooks/localStorageHook.ts
+++ src/hooks/localStorageHook.ts
@@ -1,3 +1,5 @@
+"use client ";
+
 import { LSKey } from "@/constants/localStorageKeys";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
@@ -5,7 +7,10 @@ export function useLocalStorage(key: LSKey): {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
 } {
-  const initValue = localStorage.getItem(key) || "";
+  let initValue: string | null = "";
+
+  if (!(typeof localStorage === "undefined"))
+    initValue = localStorage.getItem(key) || "";
   const [value, setValue] = useState(initValue);
 
   useEffect(() => {

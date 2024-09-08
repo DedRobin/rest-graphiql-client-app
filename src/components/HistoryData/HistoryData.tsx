180f614db59743fd2c7b874481e97f7fd6dc0a70
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-08 17:05:33 +0300

    fix: update heading in hisory page

--- src/components/HistoryData/HistoryData.tsx
+++ src/components/HistoryData/HistoryData.tsx
@@ -1,13 +1,16 @@
+import { useLocale } from "@/services/locale/contex";
 import { DataItem } from "./DataItem";
-
+import localeData from "@/services/locale/lang.json";
 export function HistoryData({
   historyData,
 }: {
   historyData: { [key: string]: string }[];
 }) {
+  const { language } = useLocale();
+
   return (
     <>
-      <h1 className="text-5xl">History Requests</h1>
+      <h1 className="text-5xl">{localeData.history.heading[language]}</h1>
       <div className="history-requests flex flex-col justify-center gap-3 hover:*:text-lime-500 hover:*:border-lime-500">
         {historyData.map((item, index) => (
           <DataItem key={index} item={item} />

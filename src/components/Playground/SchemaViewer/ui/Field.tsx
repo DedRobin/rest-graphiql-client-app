ceca325bb87922867c0b0c06a00a110436929534
vt <trachenko.v.a@gmail.com>
2024-09-10 19:25:03 +0300

    fix: update Field

--- src/components/Playground/SchemaViewer/ui/Field.tsx
+++ src/components/Playground/SchemaViewer/ui/Field.tsx
@@ -1,21 +1,58 @@
 import React from "react";
+import Image from "next/image";
+import { cn } from "@/utils/cn";
 
-export function Field({
-  name,
-  type,
-  separator = ": ",
-  className = "",
-}: {
+interface FieldProps {
   name: string;
   type: string;
   separator?: string;
   className?: string;
-}) {
-  return (
-    <p className={className}>
-      {name}
-      {separator}
-      {type}
-    </p>
-  );
+  onClick?: () => void;
+  isOpen?: boolean;
 }
+
+export const Field: React.FC<FieldProps> = ({
+  name,
+  type,
+  separator = ": ",
+  className = "",
+  onClick,
+  isOpen = false,
+}) => {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        className={cn(
+          "flex items-center justify-between px-4 py-2 w-full text-left transition-colors",
+          {
+            "bg-black": !isOpen,
+            "bg-darkGray": isOpen,
+            "text-lightGray": !isOpen,
+          },
+        )}
+      >
+        <div className="flex items-center">
+          <div className="flex flex-col">
+            <div className="flex flex-row items-center">
+              <h6 className={cn("text-lightGray", className)}>{name}</h6>
+              {separator}
+            </div>
+            <span className="text-mediumGray">{type}</span>
+          </div>
+        </div>
+        <Image
+          src={
+            isOpen
+              ? "/icons/button-right--arrow.svg"
+              : "/icons/button-left--arrow.svg"
+          }
+          alt="Toggle"
+          width={14}
+          height={14}
+          className="transition-transform"
+        />
+      </button>
+    </div>
+  );
+};

e7add5c45e36034b28c75cd41f4e4bc97f89c558
Pavel Rabekin <pavelrabekin@gmail.com>
2024-09-15 14:32:44 +0300

    fix: app navigates user to login page if token is expired 2

--- src/services/next-firebase-auth-edge/config.ts
+++ src/services/next-firebase-auth-edge/config.ts
@@ -13,8 +13,7 @@ export const serverConfig = {
     httpOnly: true,
     secure: process.env.USE_SECURE_COOKIES === "true",
     sameSite: "lax" as const,
-    // maxAge: 12 * 60 * 60 * 24,
-    maxAge: 5,
+    maxAge: 12 * 60 * 60 * 24,
   },
   serviceAccount: {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
